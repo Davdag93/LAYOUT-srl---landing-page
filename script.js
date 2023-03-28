78cdd5dd1a5e6b828c798ac3beb23d3e3d9a435e
Davide D'Agostino <davide-d.agostino@outlook.com>
2023-03-28 20:02:31 +0200

    mod. img, mediaQuery

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ cards.forEach(card => {
   const content = card.querySelector('.card-front');
 
   button.addEventListener('click', () => {
-    cards.forEach(otherCard => {
+     cards.forEach(otherCard => {
       if (otherCard !== card) {
         const otherContent = otherCard.querySelector('.card-front');
         otherContent.classList.remove('visible');
       }
-    });
+    }); 
 
     content.classList.toggle('visible');
   });
